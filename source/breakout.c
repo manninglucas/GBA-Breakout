bc24275156098b799bbd01beb70c49d3730f7608
ManningtheGoose <lucas.manning21@gmail.com>
2016-01-01 20:19:08 -0500

    a bunch of cleanup and memory optimizations

--- source/breakout.c
+++ source/breakout.c
@@ -3,16 +3,19 @@
 //
 #include "breakout.h"
 
-//checks if 2 rectangles are intersecting in the nect frame
+//Checks if 2 rectangles are intersecting in the next frame
+//Returns the side from which the intersection happens
 int will_intersect(int x1, int xv1, int y1, int yv1, int w1, int h1,
         int x2, int xv2, int y2, int yv2, int w2, int h2)
 {
     int dx1 = x1 + xv1, dy1 = y1 + yv1;
     int dx2 = x2 + xv2, dy2 = y2 + yv2;
     
-    //intersecting within the rectangle I check where it was before 
-    //the collision to determine which side it hit.
-    if ((dx1 + w1 > dx2 && dx1 < dx2 + w2) && (dy1 + h1 > dy2 && dy1 < dy2 + h2)) {
+    //First I check if the rectangles are intersecting, then extrapolate with
+    //the given parameters to estimate where the ball was in the previous frame
+    //to determine which side of the paddle collided with the ball.
+    if ((dx1 + w1 > dx2 && dx1 < dx2 + w2) 
+            && (dy1 + h1 > dy2 && dy1 < dy2 + h2)) {
         if (y1 - yv1 + h1 < y2 - yv2) 
             return TOP_COLLISION; 
         else if (y1 - yv1 > y2 - yv2 + h2)
@@ -26,7 +29,7 @@ int will_intersect(int x1, int xv1, int y1, int yv1, int w1, int h1,
 }
 
 game_obj create_game_obj(int x, int y, int w, int h, 
-        int xvel, int yvel, int hid) {
+        int xvel, int yvel) {
     game_obj tmp;
     tmp.x = x;
     tmp.y = y;
@@ -34,12 +37,12 @@ game_obj create_game_obj(int x, int y, int w, int h,
     tmp.h = h;
     tmp.xvel = xvel;
     tmp.yvel = yvel;
-    tmp.hidden = hid;
 
     return tmp;
 }
 
-
+//This resolves collisions with a moving paddle, including moving
+//x direction collisions.
 void resolve_paddle_collision(game_obj *ball, game_obj *paddle)
 {
     switch(will_intersect(ball->x, ball->xvel, ball->y, ball->yvel, 
@@ -51,31 +54,30 @@ void resolve_paddle_collision(game_obj *ball, game_obj *paddle)
         
         case TOP_COLLISION:
             ball->y = paddle->y - ball->h;
-            flip(&(ball->yvel));
+            FLIP(ball->yvel);
             break;
 
         case BOTTOM_COLLISION:
             ball->y = paddle->y + paddle->h;
-            flip(&(ball->yvel));
+            FLIP(ball->yvel);
             break;
 
         case LEFT_COLLISION:
             ball->x = paddle->x - ball->w + paddle->xvel;
-            //should always reflect velocity a certain way
-            ball->xvel = ball->xvel > 0 ? -ball->xvel : ball->xvel;
+            ball->xvel = -ABS(ball->xvel);
             break;
 
         case RIGHT_COLLISION:
             ball->x = paddle->x + paddle->w + paddle->xvel;
-            //same as the other
-            ball->xvel = ball->xvel < 0 ? -ball->xvel : ball->xvel;
+            ball->xvel = ABS(ball->xvel);
             break;
     }
 }
 
+//Checks for collisions with bricks, resolves them and removes the brick.
 void resolve_brick_collision(game_obj *ball)
 {
-    for (int i = 2; i < 5*15+2; ++i)
+    for (int i = 0; i < BRICK_ROWS*BRICK_COLUMNS; ++i)
     {
         volatile obj_attr *brick_attr = &oam_mem[i];
         
@@ -83,114 +85,154 @@ void resolve_brick_collision(game_obj *ball)
         int brick_x = brick_attr->attr1 & ATTR1_X_MASK;
 
         switch(will_intersect(ball->x, ball->xvel, ball->y, ball->yvel,
-                    ball->w, ball->h, brick_x, 0, brick_y, 0, 16, 8)) {
+                    ball->w, ball->h, brick_x, 0, brick_y, 
+                    0, BRICK_WIDTH, BRICK_HEIGHT)) {
 
             case NO_COLLISION:
                break;
             
             case TOP_COLLISION:
                 ball->y = brick_y - ball->h;
-                flip(&(ball->yvel));
+                FLIP(ball->yvel);
+                
+                //to remove bricks I just put them off screen instead of messing
+                //with the hidden attribute.
                 obj_set_pos(brick_attr, SCREEN_WIDTH, 0);
                 break;
 
             case BOTTOM_COLLISION:
-                ball->y = brick_y + 8;
-                flip(&(ball->yvel));
+                ball->y = brick_y + BRICK_HEIGHT;
+                FLIP(ball->yvel);
+
                 obj_set_pos(brick_attr, SCREEN_WIDTH, 0);
                 break;
 
             case LEFT_COLLISION:
                 ball->x = brick_x - ball->w;
-                //should always reflect velocity a certain way
-                ball->xvel = ball->xvel > 0 ? -ball->xvel : ball->xvel;
+                ball->xvel = -ABS(ball->xvel);
+
                 obj_set_pos(brick_attr, SCREEN_WIDTH, 0);
                 break;
 
             case RIGHT_COLLISION:
-                ball->x = brick_x + 16;
-                //same as the other
-                ball->xvel = ball->xvel < 0 ? -ball->xvel : ball->xvel;
+                ball->x = brick_x + BRICK_WIDTH;
+                ball->xvel = ABS(ball->xvel);
+
                 obj_set_pos(brick_attr, SCREEN_WIDTH, 0);
                 break;
        }
     } 
 }
 
+void resolve_wall_collision(game_obj *ball)
+{
+    if (ball->x + ball->w + ball->xvel > SCREEN_WIDTH) {
+        ball->x = SCREEN_WIDTH - ball->w;
+        FLIP(ball->xvel);
+    } else if (ball->x + ball->xvel < 0) {
+        ball->x = 0;
+        FLIP(ball->xvel);
+    }
+
+    if (ball->y + ball->yvel < 0) {
+        ball->y = 0;
+        FLIP(ball->yvel);
+    } else if (ball->y + ball->yvel > SCREEN_HEIGHT) {
+        //eventually replace this with death code
+        ball->y = SCREEN_HEIGHT;
+        FLIP(ball->yvel);
+    }
+}
+
 int main()
 {
     //This is the first color pallete at index zero. The palette has 16
     //colors total. Each color is 16 bits. we can have 32 palettes total. 
-    obj_pal_mem[1] = rgb15(0x1F, 0x1F, 0x1F);   //white
-    obj_pal_mem[2] = rgb15(0x1F, 0, 0);         //red
-    obj_pal_mem[5] = rgb15(0, 0x1F, 0);         //green
-    obj_pal_mem[6] = rgb15(0, 0, 0x1F);         //blue
-    obj_pal_mem[4] = rgb15(0x1F, 0x1F, 0);      //yellow
-    obj_pal_mem[3] = rgb15(0x1F, 0x10, 0);      //orange 
+    obj_pal_mem[1] = rgb15(0x1F, 0, 0);         //red
+    obj_pal_mem[2] = rgb15(0x1F, 0x10, 0);      //orange 
+    obj_pal_mem[3] = rgb15(0x1F, 0x1F, 0);      //yellow
+    obj_pal_mem[4] = rgb15(0, 0x1F, 0);         //green
+    obj_pal_mem[5] = rgb15(0, 0, 0x1F);         //blue
+    obj_pal_mem[6] = rgb15(0x1F, 0x1F, 0x1F);   //white
 
-    //write sprite tiles into the fourth tile block
-    volatile u16 *paddle_tile_mem = (u16*)tile_mem[4][1];   
-    volatile u16 *ball_tile_mem = (u16*)tile_mem[4][5];
+    //This setup code is explained when I set up the other sprites later
+    for (int i = 0; i < BRICK_ROWS; ++i) {
+        
+        //we need to space these apart by 2 because there are 2 tiles in each
+        //brick and if I just set them sequentially the object will read into
+        //the wrong color tile.
+        volatile u16* brick_tile_mem = (u16*)tile_mem[4][1+i*BRICK_TILES];
+
+        //2 tiles in each brick. There's a bit of trickery to get the colors
+        //formatted for 4bpp. These operators for index 1 will create 0x1111
+        //The indicies correspond to the indicies of the colors in the 
+        //palette memory.
+        for (int k = 0; k < BRICK_TILES * (sizeof(tile4bpp) / 2); ++k) {
+            brick_tile_mem[k] = ((i + 1) << 12) | ((i + 1) << 8) 
+                    | ((i + 1) << 4) | i + 1;
+        }
+
+       
+        for (int j = 0; j < BRICK_COLUMNS; ++j) {
+
+            volatile obj_attr* brick_attr = &oam_mem[i*BRICK_COLUMNS + j];
+            brick_attr->attr0 = ATTR0_SHAPE_WIDE;
+            brick_attr->attr1 = ATTR1_SIZE_SMALL;
+            //here since we have limited space for the tile indicies I just base
+            //the tile color off the first one in the row becuase the mas is 64 
+            //I think
+            brick_attr->attr2 = i*BRICK_TILES + 1;
+
+            //offset the bricks by 3 rows from the cieling
+            obj_set_pos(brick_attr, j*BRICK_WIDTH , 
+                    BRICK_HEIGHT*3 + i*BRICK_HEIGHT);
+        }
+
+    }
+
+    //write sprite tiles into the fourth tile block. 
+    //Blocks 0-3 are for background. We only need to make one tile here
+    //because the paddle and the ball use the same exact tile. No need to create
+    //extras. We just point to the tile in memory in attribute 2 where the base
+    //is specified. It's offset by 1 because the zero tile is still background.
+    volatile u16 *game_obj_tile_mem = 
+        (u16*)tile_mem[4][BRICK_ROWS*BRICK_TILES + 1];
    
     //alright so here is a little trickery to avoid byte writes to vram,
     //which is a big no-no on the GBA. The tile is casted to a short (aka
-    //2 byte, 16-bit) pointer. So paddle_tile}_mem is essentially an array of shorts.
-    //Becuase its an array of shorts and not of bytes, we have to divide the
-    //suze by 2 to get the correct amount of iterations. There are 4 tiles
+    //2 byte, 16-bit) pointer. So paddle_tile}_mem is essentially an array of
+    //shorts. Becuase its an array of shorts and not of bytes, we have to divide
+    //the suze by 2 to get the correct amount of iterations. There are 4 tiles
     //to loop through (32x8p) so the whole thing is multiplied by 4
     //0x2222 converts to: 0b0010001000100010
     for (int i = 0; i < 4 * (sizeof(tile4bpp) / 2); ++i) {
-        paddle_tile_mem[i] = 0x1111;
+        game_obj_tile_mem[i] = 0x6666; //bad luck...?
     }
-    
-    //same thing as above except this one is only one tile (8x8p)
-    for (int i = 0; i < sizeof(tile4bpp) / 2; ++i) {
-        ball_tile_mem[i] = 0x1111;
-    }
-
-    volatile obj_attr *paddle_attr = &oam_mem[0];
-    volatile obj_attr *ball_attr = &oam_mem[1];
+   
+    volatile obj_attr *paddle_attr = &oam_mem[BRICK_ROWS*BRICK_COLUMNS];
 
     paddle_attr->attr0 = ATTR0_SHAPE_WIDE;
     paddle_attr->attr1 = ATTR1_SIZE_MEDIUM;
 
     //this bitfield sets the id of the object and the palette bank.
-    //The pallete bank we use is at index zero so no need to se it.
-    //this sets the base tile for pallete
-    paddle_attr->attr2 = 1;
+    //The pallete bank we use is at index zero so no need to set it.
+    //this sets the base tile for pallete to build the sprite from
+    paddle_attr->attr2 = BRICK_ROWS*2 + 1;
+
+    volatile obj_attr *ball_attr = &oam_mem[BRICK_ROWS*BRICK_COLUMNS+1];
 
     ball_attr->attr0 = ATTR0_SHAPE_SQUARE;
     ball_attr->attr1 = ATTR1_SIZE_SMALL; 
-    ball_attr->attr2 = 5;
+    ball_attr->attr2 = BRICK_ROWS*2 + 1;
 
-    //five rows of bricks
-    for (int i = 0; i < 5; ++i) {
-        //15 bricks in each row
-        for (int j = 0; j < 15; ++j) {
-            volatile u16* brick_tile_mem = (u16*)tile_mem[4][6+i*15+j*2];
-
-            //2 tiles in each brick
-            for (int k = 0; k < 2 * (sizeof(tile4bpp) / 2); ++k) {
-                brick_tile_mem[k] = ((i+2) << 12) | ((i+2) << 8) | ((i+2) << 4) | (i+2);  
-            }
-
-            volatile obj_attr* brick_attr = &oam_mem[2+i*15+j];
-            brick_attr->attr0 = ATTR0_SHAPE_WIDE;
-            brick_attr->attr1 = ATTR1_SIZE_SMALL;
-            brick_attr->attr2 = 6+i*15;
-
-            obj_set_pos(brick_attr, j*16 ,24+i*8);
-        }
-
-    }
-
-    //This sets it in object and 1d mode
+    //This sets it in object and 1d mode for tiles
     REG_DISPCNT = DCNT_OBJ | DCNT_OBJ_1D;
 
     const int paddle_vel = 3;
 
-    game_obj paddle = create_game_obj(20, SCREEN_HEIGHT - 8, 32, 8, 0, 0, 0);
-    game_obj ball = create_game_obj(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 8, 8, 1, 1, 0);
+    game_obj paddle = create_game_obj(20, SCREEN_HEIGHT - 8, 32, 8, 0, 0);
+    game_obj ball = create_game_obj(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2,
+            8, 8, 1, 1);
 
     obj_set_pos(paddle_attr, paddle.x, paddle.y);
     obj_set_pos(ball_attr, ball.x, ball.y);
@@ -208,6 +250,7 @@ int main()
         while(REG_VCOUNT < 160);    // wait till VBlank
         
         key_poll();
+        paddle.x += paddle.xvel;
 
         if (key_is_down(KEY_LEFT)) {
             paddle.xvel = paddle.x + paddle.xvel < 0 ?  0 : -paddle_vel;
@@ -219,30 +262,11 @@ int main()
             paddle.xvel = 0;
         }
         
-        paddle.x += paddle.xvel;
 
         ball.x += ball.xvel;
         ball.y += ball.yvel;
 
-        if (ball.x + ball.w > SCREEN_WIDTH) {
-            ball.x = SCREEN_WIDTH - ball.w;
-            flip(&(ball.xvel));
-        } else if (ball.x < 0) {
-            ball.x = 0;
-            flip(&(ball.xvel));
-        }
-
-        if (ball.y < 0) {
-            ball.y = 0;
-            flip(&(ball.yvel));
-        }
-
-        //eventually replace this with death code
-        if (ball.y > SCREEN_HEIGHT) {
-            ball.y = SCREEN_HEIGHT;
-            flip(&(ball.yvel));
-        }
-
+        resolve_wall_collision(&ball);
         resolve_paddle_collision(&ball, &paddle); 
         resolve_brick_collision(&ball); 
 
--- source/breakout.h
+++ source/breakout.h
@@ -3,12 +3,12 @@
 //A lot of general GBA programming stuff is gonna go in here
 //as well as some "breakout" specific defines and function
 //declarations
-//
-//
+
 
 #ifndef BREAKOUT_H
 #define BREAKOUT_H
 
+//Types and basic util macros
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
@@ -17,8 +17,30 @@ typedef u16 color;//15 bit color
 typedef u32 tile4bpp[8]; //each tile 
 typedef tile4bpp tile_block[512];
 
-#define TRUE            1
-#define FALSE           0
+//Miscellaneous structs
+typedef struct game_obj {
+    int x, y, w, h;
+    int xvel, yvel;
+} game_obj;
+
+
+typedef struct obj_attr
+{
+    u16 attr0, attr1, attr2;
+    short fill;
+} __attribute__((aligned(4))) obj_attr;
+//This aligns the struct correctly for
+//the GBA memory structure
+
+#define TRUE                    1
+#define FALSE                   0
+
+#define FLIP(x) x = -x
+#define ABS(x)  x < 0 ? -x : x
+
+//Game related types and constants
+//tiles on the GBA are 8x8p
+#define TILE_SIZE               64
 
 #define NO_COLLISION            0
 #define LEFT_COLLISION          1
@@ -26,27 +48,41 @@ typedef tile4bpp tile_block[512];
 #define TOP_COLLISION           3
 #define BOTTOM_COLLISION        4
 
-typedef struct game_obj {
-    int x, y, w, h;
-    int xvel, yvel;
-    int hidden;
-} game_obj;
+#define BRICK_ROWS              5
+#define BRICK_COLUMNS           15
+#define BRICK_WIDTH             16
+#define BRICK_HEIGHT            8
+#define BRICK_SIZE              BRICK_WIDTH * BRICK_HEIGHT
+#define BRICK_TILES             BRICK_SIZE / TILE_SIZE
 
+#define PADDLE_WIDTH            32
+#define PADDLE_HEIGHT           8
+
+#define BALL_WIDTH              8
+#define BALL_HEIGHT             8
+
+#define SCREEN_WIDTH            240
+#define SCREEN_HEIGHT           160
+
+//GBA mem map stuff
 #define MEM_IO          0x04000000 //IO registers are here
 #define MEM_PAL         0x05000000 //Color palette memory
 #define MEM_VRAM        0x06000000 //screen VRAM starts here
 #define MEM_OAM         0x07000000 //object/srpite mem here
-
-#define DCNT_OBJ        0x1000
-#define DCNT_OBJ_1D     0x0040
 
 #define REG_DISPCNT     (*((volatile u32 *)(MEM_IO)))
 #define REG_DISPSTAT    (*((volatile u32 *)(MEM_IO + 0x0004)))
 #define REG_VCOUNT      (*((volatile u32 *)(MEM_IO + 0x0006)))
 #define REG_KEYINPUT    (*((volatile u32 *)(MEM_IO + 0x0130)))
 
-#define SCREEN_WIDTH    240
-#define SCREEN_HEIGHT   160
+#define oam_mem ((volatile obj_attr *)MEM_OAM)
+#define tile_mem ((volatile tile_block *)MEM_VRAM)
+#define obj_pal_mem ((volatile color *)(MEM_PAL + 0x200))
+
+//A bunch of random bitfield values/masks 
+//for various purposes
+#define DCNT_OBJ                0x1000
+#define DCNT_OBJ_1D             0x0040
 
 #define ATTR0_MODE_MASK         0x0300
 #define ATTR0_MODE_SHIFT        0x0008
@@ -65,34 +101,22 @@ typedef struct game_obj {
 #define ATTR0_Y_MASK            0x00FF 
 #define ATTR1_X_MASK            0x01FF
 
-typedef struct obj_attr
-{
-    u16 attr0, attr1, attr2;
-    short fill;
-} __attribute__((aligned(4))) obj_attr;
-
-//This aligns the struct correctly for
-//the GBA memory structure
-
-#define oam_mem ((volatile obj_attr *)MEM_OAM)
-#define tile_mem ((volatile tile_block *)MEM_VRAM)
-#define obj_pal_mem ((volatile color *)(MEM_PAL + 0x200))
+#define KEY_RIGHT               0x0010
+#define KEY_LEFT                0x0020
+#define KEY_START               0x0008
+#define KEY_MASK                0x03FF
 
 //globals for key states
 u16 __key_curr, __key_prev;
 
-#define KEY_RIGHT       0x0010
-#define KEY_LEFT        0x0020
-#define KEY_START       0x0008
-#define KEY_MASK        0x03FF
-
-
+//updates key states
 static inline void key_poll()
 {
     __key_prev = __key_curr;
     __key_curr = ~REG_KEYINPUT & KEY_MASK;
 }
 
+//various key helpers. Don't use all of them but might eventually.
 static inline u16 key_prev_state() { return __key_prev; }
 static inline u16 key_curr_state() { return __key_curr; }
 static inline u16 key_is_down(u32 key) { return __key_curr & key; } 
@@ -101,16 +125,6 @@ static inline u16 key_is_up(u32 key) { return ~__key_curr & key; }
 static inline u16 key_was_up(u32 key) { return ~__key_prev & key; } 
 static inline u16 key_transit(u32 key) { return (__key_curr ^ __key_prev) & key; }
 
-static inline void obj_hide(volatile obj_attr *obj)
-{
-    //first part resets bitfield and then ors it with appropriate bits
-    obj->attr0 = (obj->attr0 & ~ATTR0_MODE_MASK) | ATTR0_MODE_HIDE;  
-}
-
-static inline void obj_show(volatile obj_attr *obj)
-{
-    obj->attr0 = (obj->attr0 & ~ATTR0_MODE_MASK) | ATTR0_MODE_REG;  
-}
 
 static inline void obj_set_pos(volatile obj_attr *obj, int x, int y)
 {
@@ -122,10 +136,5 @@ static inline color rgb15(u32 red, u32 green, u32 blue)
 {
     return red | (green<<5) | (blue<<10);
 }
-
-static inline void flip(int *val) { *val = -(*val); }
-
-
-
 
 #endif //breakout_h
